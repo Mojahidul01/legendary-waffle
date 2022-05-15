c29ddc17252f783f0cdf4c86d27c41151749e4da
gautam8404 <gautamdhingra8404@gmail.com>
2022-05-15 18:38:42 +0530

    typos

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+import os
 from telethon import TelegramClient
 
-api_id = "4037824"
-api_hash = "088d9796ffcb8857f345d66fe484e692"
-bot_token = "2126552476:AAGjFXrV0hsz5JmKIzdJBdcDEctl02MmtIA"
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')
+bot_token = os.environ.get('BOT_TOKEN')
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
