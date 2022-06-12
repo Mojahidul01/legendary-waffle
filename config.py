d9ef0d8198ec7783fb9abd760749d08253f60b29
gautam8404 <gautamdhingra8404@gmail.com>
2022-06-12 06:55:27 +0530

    another fix

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
-from time import time
-from telethon import TelegramClient
 import os
+from telethon import TelegramClient
 
 DEFAULT_TIMEOUT = 60
 
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
-timeout = int(os.environ.get('TIMEOUT'))
+timeout = os.environ.get('TIMEOUT')
 
 if timeout == None:
     timeout = DEFAULT_TIMEOUT
+
+timeout = int(timeout)
 
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
